6695d57acc8607e0b832cdcc844a3035618bb677
malves85 <marceloa153@gmail.com>
2023-03-15 17:15:28 +0000

    Criação de frontend getAll alunos

---
+++ Frontend/src/models/students/StudentDTO.ts
@@ -0,0 +1,11 @@
+export interface StudentDTO {
+    id: number;
+    name: string;
+    email: string;
+    age: number;
+    city: string;
+    identification: number;
+    initialLevel: string;
+    currentLevel: string,
+    classId: number;
+}
